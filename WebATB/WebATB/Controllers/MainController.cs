00f78c1696b79a1c4221dfa28cd8fdb102a8cb60
novakvova <novakvova@gmail.com>
2026-01-21 10:39:38 +0200

    Being asp.net core

---
+++ WebATB/WebATB/Controllers/MainController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebATB.Models.Users;
+
+namespace WebATB.Controllers;
+
+public class MainController : Controller
+{
+    public IActionResult Index()
+    {
+        List<UserItemModel> model = new List<UserItemModel>();
+
+        model.Add(new UserItemModel
+        {
+            Id = 1,
+            Name = "Підкаблучник Марко Йосипович",
+            Phone = "+38(098)235 6421"
+        });
+        model.Add(new UserItemModel
+        {
+            Id = 2,
+            Name = "Рижий Олексій Мохнович",
+            Phone = "+38(098)230 6421"
+        });
+        model.Add(new UserItemModel
+        {
+            Id = 3,
+            Name = "Галоша Іванна Ігорівна",
+            Phone = "+38(098)236 6421"
+        });
+
+        return View(model);
+    }
+}
---
+++ WebATB/WebATB/Models/Users/UserItemModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebATB.Models.Users;
+
+public class UserItemModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Phone { get; set; } = null!;
+}
