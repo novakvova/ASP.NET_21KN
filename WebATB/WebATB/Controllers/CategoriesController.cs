f4a588aa8324ca007eac2b1bff1ae58aa1006e83
novakvova <novakvova@gmail.com>
2026-02-18 11:43:07 +0200

    View Categories

---
+++ WebATB/WebATB/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebATB.Data;
+
+namespace WebATB.Controllers;
+// робимо Injection для роботи з БД,
+// але поки що просто виводимо сторінку
+public class CategoriesController(MyContextATB myContextATB) 
+    : Controller
+{
+    public IActionResult Index()
+    {
+        var categories = myContextATB.Categories.ToList();
+        return View(categories);
+    }
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+}
---
+++ WebATB/WebATB/Models/Categories/CategoryCreateViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebATB.Models.Categories;
+
+public class CategoryCreateViewModel
+{
+    public string Name { get; set; } = null!;
+    public string Slug { get; set; } = null!;
+    public IFormFile? FileImage { get; set; }
+}
