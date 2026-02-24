ddb867e2b6a0e88877f3bab05ea0bd9224d0af75
novakvova <novakvova@gmail.com>
2026-02-24 10:15:12 +0200

    Working is good View and Create Categories

--- WebATB/WebATB/Controllers/CategoriesController.cs
+++ WebATB/WebATB/Controllers/CategoriesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebATB.Data;
+using WebATB.Data.Entities;
+using WebATB.Models.Categories;
 
 namespace WebATB.Controllers;
 // робимо Injection для роботи з БД,
@@ -16,5 +18,37 @@ public class CategoriesController(MyContextATB myContextATB)
     public IActionResult Create()
     {
         return View();
+    }
+    [HttpPost]
+    public IActionResult Create(CategoryCreateViewModel model)
+    {
+        if (ModelState.IsValid) //Зберігаємо категорію в БД, якщо модель валідна
+        {
+            string fileName = "default.jpg";
+            //Як зберегти фото
+            if (model.FileImage != null)
+            {
+                var dir = Directory.GetCurrentDirectory();
+                var wwwroot = "wwwroot";
+                fileName = Guid.NewGuid().ToString()+".jpg";
+                var savePath = Path.Combine(dir, wwwroot, "images", fileName);
+                using (var stream = new FileStream(savePath, FileMode.Create))
+                {
+                    model.FileImage.CopyTo(stream);
+                }
+            }
+            //Заповнюю таблицю категорій в БД
+            var category = new CategoryEntity
+            {
+                Name = model.Name,
+                Slug = model.Slug,
+                Image = fileName
+            };
+            myContextATB.Categories.Add(category); //Роблю SQL запит INSERT
+            myContextATB.SaveChanges(); //Зберігаю зміни в БД - Викную SQL запит COMMIT
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(model); // Якщо модель не валідна, повертаємо її назад на форму для виправлення помилок
     }
 }
--- WebATB/WebATB/Models/Categories/CategoryCreateViewModel.cs
+++ WebATB/WebATB/Models/Categories/CategoryCreateViewModel.cs
@@ -1,8 +1,16 @@
-﻿namespace WebATB.Models.Categories;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebATB.Models.Categories;
 
 public class CategoryCreateViewModel
 {
+    [Display(Name = "Вкажіть назву категорії")]
+    [Required(ErrorMessage = "Вкажіть назву категорії")]
     public string Name { get; set; } = null!;
+    [Display(Name = "Вкажіть Slug")]
+    [Required(ErrorMessage = "Вкажіть Slug")]
     public string Slug { get; set; } = null!;
+    [Display(Name = "Оберіть фото категорії")]
+    [Required(ErrorMessage = "Вкажіть фото для категорії")]
     public IFormFile? FileImage { get; set; }
 }
