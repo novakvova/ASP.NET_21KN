9aa9b754a82d46c115f3caee1e5d7eba0176c0b1
novakvova <novakvova@gmail.com>
2026-03-25 10:33:19 +0200

    Working Create Product

---
+++ WebATB/WebATB/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebATB.Controllers;
+
+public class ProductsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Create()
+    {
+        return View();
+    }
+}
