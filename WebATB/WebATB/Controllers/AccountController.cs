444f8b41be208e0111502e66e5ae4f44cb019805
novakvova <novakvova@gmail.com>
2026-04-15 14:56:41 +0300

    Add Account Model Register

---
+++ WebATB/WebATB/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebATB.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet] //Реєстрація нового користувача
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
