843c8f0df51d3f87fb101748fb3a1b4a7af25170
novakvova <novakvova@gmail.com>
2026-04-29 11:31:00 +0300

    Logout is good

--- WebATB/WebATB/Controllers/AccountController.cs
+++ WebATB/WebATB/Controllers/AccountController.cs
@@ -88,5 +88,11 @@ namespace WebATB.Controllers
             return View(model); // Якщо модель не валідна, повертаємо її назад на форму для виправлення помилок
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return Redirect("/");
+        }
     }
 }
